ad227ba838bcbc793b94a675844f69e3f8b39310
d <d@dev.ascon-kurgan.ru>
2011-02-26 02:38:33 +0800

    fix bug e-mail activation

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,11 +1,13 @@
 class UserObserver < ActiveRecord::Observer
+
   def after_create(user)
     UserMailer.deliver_signup_notification(user)
   end
 
   def after_save(user)
-  
-    UserMailer.deliver_activation(user) if user.activated_at
-  
+   # UserMailer.deliver_activation(user) if user.activated_at
   end
+
+
+
 end
